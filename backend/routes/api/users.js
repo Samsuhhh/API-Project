7c5411ce93aacc4d40bce691416f36150600fa45
Sam Suh <suhvivor24@gmail.com>
2022-08-25 18:38:38 -0700

    Add User singup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,27 @@
 // backend/routes/api/users.js
 // Create and export an Express router
-const express = require('express')
+const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+
+// Sign up route handler
+router.post('/', async (req, res) => {
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
+    }
+);
+
+
+
 
 module.exports = router;
