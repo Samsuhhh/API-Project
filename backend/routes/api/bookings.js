3f70b595c4b5c2121a630b905da37d031c0defd3
Sam Suh <suhvivor24@gmail.com>
2022-08-31 18:47:49 -0700

    Edit a booking works but env variable is not being saved by host

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -12,24 +12,104 @@ router.get('/current', requireAuth, async (req, res) => {
     const myBookings = await Booking.findAll({
         where: {
             userId: req.user.id
-        }, 
-        include:[
-            {model: Spot,
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            }}
+        },
+        include: [
+            {
+                model: Spot,
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            }
         ],
     })
 
-    return res.json({Bookings: myBookings})
-})
+    return res.json({ Bookings: myBookings })
+});
+
+
+// EDIT a Booking
+router.put('/:bookingId', requireAuth, async (req, res) => {
+    const { bookingId } = req.params;
+    const { startDate, endDate } = req.body;
+    const findBooking = await Booking.findByPk(bookingId);
+
+    if (!findBooking) {
+        return res
+            .status(404)
+            .json({
+                message: "Booking couldn't be found",
+                statusCode: res.statusCode
+            })
+    };
+
+    let currentDate = new Date();
+
+    if (findBooking.endDate < currentDate) {
+        return res
+            .status(403)
+            .json({
+                message: "Past bookings can't be modified",
+                statusCode: res.statusCode
+            })
+    };
+
+    const currentBookings = await Booking.findAll({
+        where: { spotId: findBooking.spotId }
+    });
+
+    for (let i = 0; i < currentBookings.length; i++) {
+        if (
+            // startDate >= currentBookings[i].startDate && endDate <= currentBookings[i].endDate ||
+            Date.parse(startDate) <= Date.parse(currentBookings[i].startDate) &&
+            Date.parse(endDate) >= Date.parse(currentBookings[i].endDate) ||
+
+            Date.parse(startDate) >= Date.parse(currentBookings[i].startDate) &&
+            Date.parse(startDate) <= Date.parse(currentBookings[i].endDate) ||
+
+            Date.parse(endDate) >= Date.parse(currentBookings[i].startDate) &&
+            Date.parse(endDate) <= Date.parse(currentBookings[i].endDate)
+        ) {
+            return res
+                .status(403)
+                .json({
+                    message: "Sorry this spot is already booked for the specified dates",
+                    statusCode: res.statusCode,
+                    errors: {
+                        startDate: "Start date conflicts with an existing booking",
+                        endDate: "End date conflicts with an existing booking"
+                    }
+                })
+        };
+    };
+
+    try {
+        await findBooking.update({
+            startDate: startDate,
+            endDate: endDate
+        });
+
+        return res.json(findBooking);
+
+    } catch (error) {
+        return res
+            .status(400)
+            .json({
+                message: "Validation Error",
+                statusCode: 400,
+                errors: {
+                    endDate: "endDate cannot come before startDate"
+                }
+            });
+    }
+});
 
 
 
 
 
+// Delete a Booking
 router.delete('/:bookingId', requireAuth, async (req, res) => {
-    const {bookingId} = req.params; 
+    const { bookingId } = req.params;
     const findBooking = await Booking.findByPk(bookingId)
 
     if (!findBooking) {
@@ -47,7 +127,7 @@ router.delete('/:bookingId', requireAuth, async (req, res) => {
     let currentDate = `${year}-${month}-${day}`;
 
     // cannot delete a booking that has already started
-    if ( findBooking.startDate <= currentDate ){
+    if (findBooking.startDate <= currentDate) {
         return res
             .status(404)
             .json({
