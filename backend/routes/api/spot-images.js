83b606c47d7359bb8f79ee85e4bc218dc8012a60
Sam Suh <suhvivor24@gmail.com>
2022-08-31 19:32:58 -0700

    styled deleting a spotImage

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -3,9 +3,33 @@ const { SpotImage } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send('HELLO')
-})
+
+// Router is live test
+// router.get('/', async (req, res) => {
+//     res.send('HELLO')
+// })
+
+router.delete('/:imageId', requireAuth, async (req, res) => {
+    const {imageId} = req.params;
+    const deleteImage = await SpotImage.findByPk(imageId);
+
+    if(!deleteImage){
+        return res
+                .status(404)
+                .json({
+                    message: "Spot Image couldn't be found",
+                    statusCode: res.statusCode
+                });
+    };
+
+    deleteImage.destroy();
+    res.statusCode = 200
+    return res.json({
+        message: 'Successfully deleted',
+        statusCode: res.statusCode
+    });
+
+});
 
 
 
