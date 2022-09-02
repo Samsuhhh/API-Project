ef0cdfe1a1a8ff5be67408a04114984505cdc699
Sam Suh <suhvivor24@gmail.com>
2022-09-01 21:05:48 -0700

    added Preview Image for Get all Reviews of Current User endpoint

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -39,8 +39,18 @@ router.get('/current', requireAuth, async (req, res) => {
         },
     });
 
+    for (let i = 0; i < reviews.length; i++) {
+        let resPreview = reviews[i].toJSON();
+        let spotImageUrl = resPreview.Spot.SpotImages[0]
 
-
+        if (spotImageUrl) {
+            resPreview.Spot.previewImage = spotImageUrl.url
+        } else {
+            resPreview.Spot.previewImage = 'No Image Available'
+        }
+        delete resPreview.Spot.SpotImages;
+        reviews[i] = resPreview;
+    };
 
     return res.json({ Reviews: reviews })
 });
