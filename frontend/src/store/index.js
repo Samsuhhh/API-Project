ac26a96874829cdfd575c1a32a9bf7495f246612
Sam Suh <suhvivor24@gmail.com>
2022-09-15 22:13:43 -0700

    frontend initial setup

---
+++ frontend/src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+
+const rootReducer = combineReducers({
+  // add reducer functions here
+
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === "production") {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
