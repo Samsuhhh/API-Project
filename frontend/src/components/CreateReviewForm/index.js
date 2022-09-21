68515262b90a135b7c21c7c6c7b7fa968f303cb4
Sam Suh <suhvivor24@gmail.com>
2022-09-20 19:22:07 -0700

    create Review form created

--- frontend/src/components/CreateReviewForm/index.js
+++ frontend/src/components/CreateReviewForm/index.js
@@ -7,6 +7,44 @@ import { createReview } from "../../store/reviews";
 
 const CreateReviewForm = () => {
 
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const params = useParams();
+    const { spotId } = params;
+
+    const [review, setReview] = useState('');
+    const [stars, setStars] = useState(0);
+
+    return (
+        <div id='create-review-form'>
+            <div>
+                <form>
+                    <div>
+                        <label htmlFor="review"></label>
+                        <textarea
+                            id='review'
+                            type='text'
+                            onChange={e => setReview(e.target.value)}
+                            value={review}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="stars"></label>
+                        <input
+                            id='stars'
+                            type='number'
+                            min='0'
+                            max='5'
+                            step='1'
+                            value={stars}
+                            onChange={e => setStars(e.target.value)}
+                        />
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    )
 
 
 }
