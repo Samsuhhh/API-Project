20f25ad561802ecc3c66814e0c7b82c2875893cd
Sam Suh <suhvivor24@gmail.com>
2022-09-25 03:01:32 -0700

    spotInformation CSS done

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -16,26 +16,32 @@ const SingleSpot = (spot) => {
             <div className="spot-card-container">
                 <div className='image-section'>
                     <img id='spot-img' alt='beautiful spotImage' src={spot.spot.previewImage ||
-                        'https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg'} 
-                        />
+                        'https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg'}
+                    />
                 </div>
-                <div id='location-avgRating' className='spot-values'>
-                    <div id='location'>
-                        {`${spot.spot.city}, ${spot.spot.state}`}
+                <div id='singleSpot-details-container'>
+                    <div id='location-avgRating' className='spot-values'>
+                        <div id='location'>
+                            {`${spot.spot.city}, ${spot.spot.state}`}
+                        </div>
+                        <div className='spot-values'>
+                            {'★ ' + spot.spot.avgRating}
+                        </div>
                     </div>
                     <div className='spot-values'>
-                        {spot.spot.avgRating}
+                        {spot.spot.name}
+                    </div>
+                    <div className='spot-values'>
+                        {spot.spot.address}
+                    </div>
+                    <div className='spot-values'>
+                        <span style={{ fontWeight: '600' }}>
+                        ${spot.spot.price}
+                        </span>
+                        <span style={{paddingLeft:'3px'}}>night</span>
                     </div>
                 </div>
-                <div className='spot-values'>
-                    {spot.spot.name}
-                </div>
-                <div className='spot-values'>
-                    {spot.spot.address}
-                </div>
-                <div className='spot-values'>
-                    ${spot.spot.price}
-                </div>
+
             </div>
         </NavLink>
         // </> 
--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -20,9 +20,7 @@ const SpotDetail = () => {
     // delete spotImages at the end of useSelector and then put it back in for the BUG to occur
     // const reviewUser = useSelector(state => state.reviews.spot);
 
-    const spotDetails = useSelector(state => {
-        return state.spots.singleSpot
-    });
+    const spotDetails = useSelector(state => state.spots.singleSpot);
     // console.log('goodbye', spotImg)
     console.log('SPOT DETAILS', spotDetails.SpotImages)
 
@@ -81,7 +79,7 @@ const SpotDetail = () => {
         <div id='spot-outermost'>
             <div id='header-wrap'>
                 <div id='spotName-header'>
-                    <h1> Spot Name:  {spotDetails.name}</h1>
+                    <h2 style={{ fontWeight: 500 }}> Spot Name:  {spotDetails.name}</h2>
                 </div>
             </div>
             <div id='spotPage'>
@@ -104,54 +102,137 @@ const SpotDetail = () => {
                                 <span></span>
                             </div>
                         </div>
+                        <div id='host-info-div'>
+                            <div id='host-info-container'>
+                                <img
+                                    alt='enter icon'
+                                    src="https://www.pngrepo.com/png/318342/180/lock.png"
+                                    style={{
+                                        width: '30px',
+                                        height: '30px',
+                                        paddingTop: '20px',
 
-                        <div className='host-information'>
-                            Self check-in
-                        </div>
-                        <div className='host-information'>
-                            <div>
-                                {spotDetails.Owner?.firstName} is a Superhost
+                                    }}
+                                />
+                                <div className='host-information'>
+                                    <div>
+                                        Self check-in
+                                    </div>
+                                    <div id='Check-in-description'>
+                                        Check yourself in with the keypad.
+                                    </div>
+                                </div>
                             </div>
-                            <div id='superhost-description'>
-                                Superhosts are experienced, highly rated hosts who are committed to making money by appealing to guests.
-                                hell yea I got a lump in throught yea and you're going to sing the words wrong
+
+                            <div id='host-superhost-div'>
+                                <img
+                                    src='https://www.pngrepo.com/png/128236/180/badge.png'
+                                    alt='superhost-badge'
+                                    style={{
+                                        width: '30px',
+                                        height: '30px',
+                                        paddingTop: '23px',
+                                        paddingLeft: '2px',
+                                    }}
+                                />
+                                <div id='aa-guarantee-div' className='host-information'>
+
+                                    <div>
+                                        {spotDetails.Owner?.firstName} is a Superhost
+                                    </div>
+                                    <div id='superhost-description'>
+                                        Superhosts are experienced, highly rated hosts who are committed to making money by appealing to guests.
+                                        hell yea I got a lump in throught yea and you're going to sing the words wrong
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className='host-information'>
-                            Free cancellation for 48 hours.
+                            <div id='free-cancel-div'>
+                                <img
+                                    src='https://www.pngrepo.com/png/56251/180/calendar.png'
+                                    alt='superhost-badge'
+                                    style={{
+                                        width: '30px',
+                                        height: '30px',
+                                        paddingTop: '23px',
+                                        paddingLeft: '4px'
+
+                                    }}
+                                />
+                                <div className='host-information'
+                                    style={{
+                                        fontSize: '19px',
+                                        fontWeight: '500',
+                                        paddingTop: '3px'
+                                    }}
+                                >
+                                    Free cancellation for 48 hours.
+                                </div>
+                            </div>
                         </div>
 
-                        <div className='host-information' id='appAcademy-disclaimer'>
-                            <logo>aA logo</logo>
-                            <div>
+                        <div id='appAcademy-disclaimer'>
+                            <logo>
+                                <img
+                                    alt="app academy logo"
+                                    style={{ width: '210px', height: '70px' }}
+                                    src='https://assets-global.website-files.com/5dcc7f8c449e597ed83356b8/6269b3a19f67fd137a262d0a_A%20Logo%20Main%20-%20Red.svg' />
+                            </logo>
+                            <div style={{ paddingLeft: '3px', paddingBottom: '3px' }}>
                                 Everything on this site is 1000% real, venmo: @Samsuhhh for payments.
                             </div>
                             <div>
-                                <Link color='black' to='/https://www.appacademy.io/'>Learn More</Link>
+                                <a style={{ color: 'black', paddingLeft: '3px', paddingBottom: '3px' }}
+                                    href='/https://www.appacademy.io/'>
+                                    Learn More
+                                </a>
                             </div>
                         </div>
 
-                        <div id='spot-description'className='host-information'>
+                        <div id='spot-description' className='host-information'>
                             Spot Description: {spotDetails.description}
                         </div>
 
 
                     </div>
+
                     <div id='right-modal'>
-                        {currentUser && currentUser.id === spotDetails.ownerId && (
-                            <button onClick={updateRedirect}>UPDATE SPOT</button>
-                        )}
-                        <br></br>
-                        {currentUser && currentUser.id === spotDetails.ownerId && (
-                            <button onClick={deleteHandler}>DELETE</button>
-                        )}
+                        <div id='right-modal-content'>
+                            <div id='modal-header'>
+                                <div id='header-price'>
+                                    <span id='header-left-price'>
+                                        {'$' + spotDetails.price}
+                                    </span>
+                                    <span id="header-left-night">
+                                        night
+                                    </span>
+                                </div>
+                                <div id='header-right-stars-reviews'>
+                                    <span id='headerRight-reviews'>
+                                        {spotDetails.avgRating === null ? 'Un-visited' : '★ ' + spotDetails.avgRating}
+                                    </span>
+                                    &#x2022;
+                                    <span id='headerRight-stars'>
+                                        {spotDetails.numReviews} reviews
+                                    </span>
+                                </div>
+                            </div>
+
+                            {currentUser && currentUser.id === spotDetails.ownerId && (
+                                <button onClick={updateRedirect}>UPDATE SPOT</button>
+                            )}
+
+                            <br></br>
+                            {currentUser && currentUser.id === spotDetails.ownerId && (
+                                <button onClick={deleteHandler}>DELETE</button>
+                            )}
+                        </div>
                     </div>
 
                 </div>
             </div>
             <section id='reviews-container'>
                 {/* <div> */}
-                    <SpotReviews />
+                <SpotReviews />
                 {/* </div> */}
                 <div>
                     <button onClick={newReviewRedirect}>
