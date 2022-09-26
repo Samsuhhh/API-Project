551b83204ddf89c74808209e7d217af7537e1c87
Sam Suh <suhvivor24@gmail.com>
2022-09-25 19:44:20 -0700

    nav bar styled and stars added to create a review

--- frontend/src/components/CreateReviewForm/index.js
+++ frontend/src/components/CreateReviewForm/index.js
@@ -44,7 +44,7 @@ const CreateReviewForm = () => {
                 <h1 id='review-h1'>Leave a Review</h1>
                 <form onSubmit={submitHandler}>
                     <div>
-                        <div>How was your stay?</div>
+                        <div style={{fontSize: '18px', fontWeight: '550'}}>How was your stay?</div>
                         <label htmlFor="review"></label>
                         <textarea style={{ resize: 'none', outline:'none', width:'360px', height:'150px'}}
                             id='review-description'
@@ -53,8 +53,8 @@ const CreateReviewForm = () => {
                             value={review}
                         />
                     </div>
-                    <div> 
-                        <label htmlFor="stars"></label>
+                    <div id='starsContainer'> 
+                        {/* <label htmlFor="stars"></label>
                         <input
                             id='stars-reviews'
                             type='number'
@@ -63,9 +63,19 @@ const CreateReviewForm = () => {
                             step='1'
                             value={stars}
                             onChange={e => setStars(e.target.value)}
-                        />
-                    </div>
+                        /> */}
 
+                        <div id='starHeading'>Rate your stay</div>
+                        <fieldset id='fieldset-stars'class="rate" value={stars} onChange={e => setStars(e.target.value)}>
+                            <input className="starInput" type="radio" id="rating10" name="rating" value="5" /><label for="rating10" title="5 stars"></label>
+                            <input className="starInput" type="radio" id="rating8" name="rating" value="4" /><label for="rating8" title="4 stars"></label>
+                            <input className="starInput" type="radio" id="rating6" name="rating" value="3" /><label for="rating6" title="3 stars"></label>
+                            <input className="starInput" type="radio" id="rating4" name="rating" value="2" /><label for="rating4" title="2 stars"></label>
+                            <input className="starInput" type="radio" id="rating2" name="rating" value="1" /><label for="rating2" title="1 star"></label>
+                        </fieldset>
+                    </div>
+                    {/* <br></br>
+                        {stars} stars */}
                     <div id='createReview-buttonDiv'>
                     <button id='createReview-submitBtn'type='submit'>Submit your review</button>
                     <button id='cancelReviewbtn'onClick={handleCancel}>Cancel</button>
