3954bb743c23ef140b5b288ff0e354c63ef813fc
Samsuhhh <suhvivor24@gmail.com>
2023-04-13 15:30:58 -0700

    creating nav bar branch with component files

---
+++ frontend/src/components/NavIconBar/NavIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const NavIcon = () => {
+
+
+
+    return (
+        <div>
+            <span>
+                <img src="https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg" alt="Beachfront" />
+                <div>Beachfront</div>
+            </span>
+        </div>
+    )
+}
---
+++ frontend/src/components/NavIconBar/NavIconBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavIconBar = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
